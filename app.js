1e25ea4981ecc462b778d10584e8907eab1efefe
unknown <Muhammad Hassan Raza>
2024-05-25 02:13:00 +0500

    JS stopwatch

--- app.js
+++ app.js
@@ -5,13 +5,22 @@ var sec = 0
 function start() {
    timer = setInterval(() => {
       sec++
-     if(sec >= 60){
+      if(sec<10){
+        sec = '0'+sec
+      }
+     else if(sec >= 60){
         min++
         sec = 0
+        if(min<10){
+          min = '0'+min
+        }
       }
-      else if(min >= 60){
+     else if(min >= 60){
         hour++
         min = 0
+        if(hour<10){
+          hour = '0'+hour
+        }
       }
      document.getElementById('numbers').innerHTML = `${hour} : ${min} : ${sec}`
   }, 1000);
@@ -29,6 +38,6 @@ function reset() {
     pause()
 }
 
-function o(){
+function ops(){
    alert("please don't dobble click ❗ \nnow refresh 🔄 \nand then click")
 }
